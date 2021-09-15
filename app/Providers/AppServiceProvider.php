691c297cefebbb84f31582121b01110921d03c3f
dangphuongnam <nam.dp.501@aptechlearning.edu.vn>
2021-09-16 04:06:42 +0700

    'a'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use \Illuminate\Support\Facades\URL;
+use URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
                 Paginator::useBootstrap();
     }
 }
