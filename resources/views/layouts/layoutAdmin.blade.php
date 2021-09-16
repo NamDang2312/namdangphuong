b6a176ba00af39e15c32424defdd9bf28f43d266
dangphuongnam <nam.dp.501@aptechlearning.edu.vn>
2021-09-16 19:03:40 +0700

    'a'

--- resources/views/layouts/layoutAdmin.blade.php
+++ resources/views/layouts/layoutAdmin.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('public/ForLayoutAdmin') }}/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ url('public/ForLayoutAdmin') }}/plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
